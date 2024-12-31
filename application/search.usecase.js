78036328a939c51093bcb91ec1e24c398ec118ad
EFS Helper <askmrtillman@gmail.com>
2024-12-30 19:16:05 -0500

    combine fuel stop and search result objects, delete unneeded members

--- application/search.usecase.js
+++ application/search.usecase.js
@@ -3,11 +3,22 @@ class SearchUseCase {
         this.fuel_solution = null;
         this.search_service = null;
     }
-    async execute(){
+    async execute() {
         this.fuel_solution.read();
-        const _search_phrases = this.fuel_solution.fuel_stops
-            .reduce((phrases, stop) => [stop.search_phrase, ...phrases], []);
-        return await this.search_service.findPlaces(_search_phrases);
+        const _self = this;
+        const results = this.fuel_solution.fuel_stops.map((stop) => {
+            return _self.search_service
+                .findPlace(stop.search_phrase)
+                .then(result => {
+                    delete result.name;
+                    delete stop.name;
+                    delete stop.line_1;
+                    delete stop.line_2;
+                    delete stop.search_phrase;
+                    return { ...result, ...stop }
+                })
+        }, [])
+        return Promise.all(results);
     }
 }
 
