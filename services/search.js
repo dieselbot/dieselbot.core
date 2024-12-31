c7f8c2b93db691f94338340f5475549f0923525a
EFS Helper <askmrtillman@gmail.com>
2024-12-31 11:31:58 -0500

    remove findplaces method

--- services/search.js
+++ services/search.js
@@ -9,15 +9,7 @@ class SearchService {
         const response = await this._api.find_place(search_phrase);
         return _format_place_response(response);
     }
-    async findPlaces(search_phrases){
-        if (!search_phrases || ! search_phrases.length) return {}
-
-        return await Promise.all(search_phrases.map(phrase => {
-            return this.findPlace(phrase)
-        }))
-    }
 }
-
 
 function _format_place_response(place_response){
     const response = { name: '', address: '' };
