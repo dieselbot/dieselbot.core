00d17e769206f8546b2f889bccb31b76b57e9ec2
EFS Helper <askmrtillman@gmail.com>
2024-12-31 19:31:17 -0500

    sanity check code for fuel stop service

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const FuelSolution = require('./domain/fuel.solution');
 const SearchService = require('./services/search');
 const SearchUseCase = require('./application/search.usecase');
-const { FUELSTOP } = require('./common/constants.json');
+// const { FUELSTOP } = require('./common/constants.json');
 const getRedisClient = require('./common/getRedisClient');
+const FuelStopService = require('./services/fuelstop');
 
 async function search(fuel_solution_text) {
     const search_usecase = new SearchUseCase();
     search_usecase.fuel_solution = new FuelSolution(fuel_solution_text);
     search_usecase.search_service = new SearchService();
     const results = await search_usecase.execute();
+
+    const service = new FuelStopService();
+
+    service.post(results);
     
-    const client = await getRedisClient();
+    // const client = await getRedisClient();
     
-    await client.publish(FUELSTOP.FOUND, JSON.stringify(results));
-    
-    await client.disconnect()
+    // await client.publish(FUELSTOP.FOUND, JSON.stringify(results));
+
+    // await client.disconnect()
 
     return results;
 }
