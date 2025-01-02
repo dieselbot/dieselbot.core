623cdb4a36494b2bd40e4a20109d4d2e102f948e
EFS Helper <askmrtillman@gmail.com>
2025-01-01 22:05:07 -0500

    store fuel stops as key/value pairs instead of array

--- domain/fuel.solution.js
+++ domain/fuel.solution.js
@@ -3,7 +3,7 @@ const FuelStop = require('./fuel.stop.js');
 class FuelSolution {
     constructor(fuel_solution_text) {
         this.text = fuel_solution_text;
-        this.fuel_stops = [];
+        this.fuel_stops = {};
     }
     read() {
         const _text = this.text;
@@ -14,13 +14,11 @@ class FuelSolution {
 
         _lines.forEach((line, i) => {
             if (/QTY:\s[A-Z|\d]+/.test(line)) {
-                this.fuel_stops.push(
-                    new FuelStop(line, _lines[i + 1])
-                )
+                this.fuel_stops[i] = new FuelStop(line, _lines[i + 1]);
+                this.fuel_stops[i].read();
             }
         })
 
-        this.fuel_stops.forEach(stop => stop.read());
     }
 }
 
