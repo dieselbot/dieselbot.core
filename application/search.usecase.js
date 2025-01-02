da6b905b19e4e6dc1804822f6c3ee1cdab69c627
EFS Helper <askmrtillman@gmail.com>
2025-01-01 22:16:07 -0500

    search database before google places api; cache missing fuel stops not found in database

--- application/search.usecase.js
+++ application/search.usecase.js
@@ -1,23 +1,32 @@
+const GooglePlacesService = require('../services/google.places');
+const FuelStopRepo = require('../repository/fuelstop.repo');
+
 class SearchUseCase {
     constructor() {
         this.fuel_solution = null;
-        this.search_service = null;
+        this.places_service = new GooglePlacesService();
+        this.fuel_stop_repo = new FuelStopRepo();
+        this.missing_fuel_stops = [];
     }
     async execute() {
         this.fuel_solution.read();
-        const _self = this;
-        const results = this.fuel_solution.fuel_stops.map((stop) => {
-            return _self.search_service
-                .findPlace(stop.search_phrase)
-                .then(result => {
-                    delete result.name;
-                    delete stop.name;
-                    delete stop.line_1;
-                    delete stop.line_2;
-                    delete stop.search_phrase;
-                    return { ...result, ...stop }
-                })
-        }, [])
+        const fuelStops = this.fuel_solution.fuel_stops;
+        const results = [];
+
+        for (const id in fuelStops) {
+            const fuelStop = fuelStops[id];
+
+            let result = await this.fuel_stop_repo.findOne(fuelStop);
+
+            if (!result) {
+                result = await this.places_service.findPlace(fuelStop.search_phrase);
+                result = { ...result, ...fuelStop.dto };
+                this.missing_fuel_stops.push(result);
+            }
+
+            results.push(result);
+        }
+
         return Promise.all(results);
     }
 }
