24ec203e05b866b447602aa28774bc27a49a1deb
EFS Helper <askmrtillman@gmail.com>
2025-01-01 23:56:23 -0500

    add rudimentary validation function + minor formatting adjustments

--- domain/fuel.stop.js
+++ domain/fuel.stop.js
@@ -15,11 +15,11 @@ class FuelStop {
         let _exec_exit = /EX: \d+/.exec(this.line_2);
         const is_magee = /magee\s+ms/gi.test(this.line_2);
 
-        if(is_magee) {
+        if (is_magee) {
             _exec_hwy = /U \d\d/.exec(this.line_1);
             _exec_exit = /EX:/.exec(this.line_2);
         };
-        
+
         const _city_state = this.line_2.substring(0, _exec_exit.index).trim().replace(/\s+/g, ' ');
         const _city_state_midpoint = _city_state.lastIndexOf(' ');
 
@@ -32,10 +32,10 @@ class FuelStop {
         this.name = normalize.name(this.name);
         this.city = normalize.city(this.city);
         this.code = get_code(this.name);
-        
+
         this.search_phrase = `${this.name} ${this.city} ${this.state} ${this.highway} exit ${this.exit}`;
     }
-    get dto(){
+    get dto() {
         const { code, city, state, highway, exit } = this;
         return { code, city, state, highway, exit };
     }
@@ -63,21 +63,28 @@ const normalize = {
     }
 }
 
-function get_code(name){
+function get_code(name) {
     switch (true) {
         case /love/gi.test(name):
             return "love";
         case /pilot/gi.test(name):
-                return "pilot";
+            return "pilot";
         case /flying/gi.test(name):
             return "flying";
         case /petro/gi.test(name):
             return "petro";
         case /(tulsa terminal|laredo terminal)/gi.test(name):
-                return "melton";
+            return "melton";
         default:
             return null;
     }
+}
+
+FuelStop.isValid = function (fuelstop) {
+    const { code, display_name } = fuelstop;
+    if(!code || !display_name) return false;
+    const code_rgx = new RegExp(code, 'i');
+    return code_rgx.test(display_name);
 }
 
 module.exports = FuelStop;
