a3afb6f8908f2bbf18d6b880fc576cb53a8e2885
EFS Helper <askmrtillman@gmail.com>
2025-01-04 18:50:47 -0500

    refactor + simplify read method; remove isValid method; format document

--- domain/fuel.stop.js
+++ domain/fuel.stop.js
@@ -2,6 +2,8 @@ const { chop_left } = require("../common/utils");
 
 class FuelStop {
     constructor(text_line_1, text_line_2) {
+        if(!text_line_1) throw new Error("missing line 1");
+        if(!text_line_2) throw new Error("missing line 2");
         this.line_1 = text_line_1;
         this.line_2 = text_line_2;
         this.city = '';
@@ -10,16 +12,16 @@ class FuelStop {
         this.exit = null;
         this.search_phrase = '';
     }
-    
+
     #_city = null;
     #_name = null;
 
-    set city(value){
+    set city(value) {
         this.#_city = chop_left.call(value, "/");
     }
     get city() { return this.#_city; }
 
-    get code() { 
+    get code() {
         switch (true) {
             case /love/gi.test(this.#_name):
                 return "love";
@@ -33,10 +35,10 @@ class FuelStop {
                 return "melton";
             default:
                 return null;
-        }    
+        }
     }
 
-    set name(value){
+    set name(value) {
         switch (true) {
             case /(pilot\s\#)/gi.test(value):
                 this.#_name = "PILOT TRAVEL CE";
@@ -47,41 +49,38 @@ class FuelStop {
         }
     }
     get name() { return this.#_name; }
-    
+
     get dto() {
         const { code, city, state, highway, exit } = this;
         return { code, city, state, highway, exit };
     }
 
-    read() {
-        let _exec_hwy = /[A-Z]+ \d\d/.exec(this.line_1);
-        let _exec_exit = /EX:.*/.exec(this.line_2);
+    read_line_1() {
+        const _exec_hwy = /[I|U]+ \d\d/.exec(this.line_1);
+        if (!_exec_hwy) throw new Error("missing highway info");
+        this.name = this.line_1.substring(0, _exec_hwy.index).trim();
+        this.highway = _exec_hwy[0].replace(' ', '-');
+    }
 
+    read_line_2() {
+        const _exec_exit = /EX:.*/.exec(this.line_2);
+        if (!_exec_exit) throw new Error("missing exit info");
         const _city_state = this.line_2.substring(0, _exec_exit.index).trim().replace(/\s+/g, ' ');
         const _city_state_midpoint = _city_state.lastIndexOf(' ');
-
-        this.name = this.line_1.substring(0, _exec_hwy.index).trim();
         this.city = _city_state.substring(0, _city_state_midpoint).trim();
         this.state = _city_state.substring(_city_state_midpoint).trim();
-        this.highway = _exec_hwy[0].replace(' ', '-');
-        
-        if(_exec_exit[0].match(/\d+/)){
-            this.exit = _exec_exit[0].match(/\d+/)[0];
+
+        if (_exec_exit[0].match(/\d+.*/)) {
+            this.exit = _exec_exit[0].match(/\d+.*/)[0];
         }
-
-        this.search_phrase = `${this.name} ${this.city} ${this.state} ${this.highway}`;
-        
-        if(this.exit) this.search_phrase = this.search_phrase.concat(` exit ${this.exit}`);
-        
     }
-    
-}
 
-FuelStop.isValid = function (fuelstop) {
-    const { code, display_name, city, state, highway } = fuelstop;
-    if (!code || !display_name || !city || !state || !highway) return false;
-    const code_rgx = new RegExp(code, 'i');
-    return code_rgx.test(display_name);
+    read() {
+        this.read_line_1();
+        this.read_line_2();
+        this.search_phrase = `${this.name} ${this.city} ${this.state} ${this.highway}`;
+    }
+
 }
 
 module.exports = FuelStop;
