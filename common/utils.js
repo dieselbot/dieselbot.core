12721a55c987b3b877f3fde85dda19da64e05129
EFS Helper <askmrtillman@gmail.com>
2025-01-04 17:12:12 -0500

    unit test chop_left; minor refactor

--- common/utils.js
+++ common/utils.js
@@ -1,7 +1,9 @@
 function chop_left(char) {
+    const string_value = this.valueOf();
+    if(!char) return string_value;
     return (new RegExp(char)).test(this)
     ? this.substring(0, this.indexOf(char)) 
-    : this.valueOf();
+    : string_value;
 }
 
 module.exports = {
--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,7 +1,50 @@
-function sum(a,b){
-    return a + b;
-}
+const { chop_left } = require("../common/utils");
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1,2)).toBe(3);
+describe('chop_left string utility', () => {
+    
+    String.prototype.chop_left = chop_left;
+
+    it('should drop all characters following the input character', () => {
+        const string_value = "123/456";
+        const input_character = "/";
+
+        const result = string_value.chop_left(input_character);
+
+        expect(result).toEqual("123");
+    })
+
+    it('should return empty string when the string value is empty', () => {
+        const string_value = "";
+        const input_character = "/";
+
+        const result = string_value.chop_left(input_character);
+
+        expect(result).toEqual("");
+    })
+
+    it('should return original string value when the input character is an empty string', () => {
+        const original_string_value = "abc/def";
+        const input_character = '';
+
+        const result = original_string_value.chop_left(input_character);
+
+        expect(result).toBe(original_string_value);
+    })
+
+    it('should return original string value when the input character is null', () => {
+        const original_string_value = "abc/def";
+        const input_character = null;
+
+        const result = original_string_value.chop_left(input_character);
+
+        expect(result).toBe(original_string_value);
+    })
+
+    it('should return original string value when the input character is undefined', () => {
+        const original_string_value = "abc/def";
+
+        const result = original_string_value.chop_left();
+
+        expect(result).toBe(original_string_value);
+    })
 })
