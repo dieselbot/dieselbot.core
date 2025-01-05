74cd8eebefd26fef4512de71d7296204f30c2ea7
EFS Helper <askmrtillman@gmail.com>
2025-01-05 15:15:58 -0500

    implement add many method; validate fuel stop before adding to database collection

--- repository/fuelstop.repo.js
+++ repository/fuelstop.repo.js
@@ -1,26 +1,30 @@
-const FuelStop = require("../domain/fuel.stop");
+const { FuelStopValidator } = require("../common/validators");
 const FuelStopDB = require("../infrastructure/fuelstop.db");
 
 class FuelStopRepo {
-    
+
     #_fuelStopDB;
 
-    constructor(fuelStopDB = new FuelStopDB()) {
+    constructor(
+        fuelStopDB = new FuelStopDB(),
+        fuelStopValidator = new FuelStopValidator()
+    ) {
         this.#_fuelStopDB = fuelStopDB;
         this.fuelstops_collection = this.#_fuelStopDB.firestore.collection("fuelstops");
+        this.fuelStopValidator = fuelStopValidator;
     }
     async findOne(fuelstop) {
 
         const { code, city, state, highway, exit } = fuelstop;
         const snapshot = await this.fuelstops_collection
-                                   .where("exit", "==", exit)
-                                   .where("highway", "==", highway)
-                                   .where("city", "==", city)
-                                   .where("state", "==", state)
-                                   .where("code", "==", code)
-                                   .orderBy('exit')
-                                   .orderBy('highway')
-                                   .get();
+            .where("exit", "==", exit)
+            .where("highway", "==", highway)
+            .where("city", "==", city)
+            .where("state", "==", state)
+            .where("code", "==", code)
+            .orderBy('exit')
+            .orderBy('highway')
+            .get();
 
         if (snapshot.empty) {
             console.warn('no matching fuel stops.');
@@ -36,8 +40,21 @@ class FuelStopRepo {
     }
 
     async addOne(fuelstop) {
-        const docRef = await this.fuelstops_collection.add(fuelstop);
-        return docRef.id;
+        if (!this.fuelStopValidator.validate(fuelstop)) {
+            throw new Error(`fuel stop validation failed: ${JSON.stringify(fuelstop)}`)
+        }
+        return await this.fuelstops_collection.add(fuelstop);
+    }
+
+    async addMany(fuelstops) {
+        for (const fuelstop of fuelstops) {
+            if (!this.fuelStopValidator.validate(fuelstop)) {
+                throw new Error(`fuel stop validation failed: ${JSON.stringify(fuelstop)}`)
+            }
+        }
+        return Promise.all(fuelstops.map((fuelstop) => {
+            return this.fuelstops_collection.add(fuelstop);
+        }))
     }
 }
 
