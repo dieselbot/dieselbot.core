3c9792f0aa6d9ef859f05ce870716adb202dff6e
EFS Helper <askmrtillman@gmail.com>
2025-01-05 12:21:21 -0500

    move utils test to same directory; remove tests directory

--- common/utils.test.js
+++ common/utils.test.js
@@ -1,16 +1,16 @@
-const { chop_left } = require("../common/utils");
+const { chop_left } = require("./utils");
 
 describe('chop_left string utility', () => {
     
     String.prototype.chop_left = chop_left;
 
     it('should drop all characters following the input character', () => {
-        const string_value = "123/456";
+        const string_value = "a/b";
         const input_character = "/";
 
         const result = string_value.chop_left(input_character);
 
-        expect(result).toEqual("123");
+        expect(result).toEqual("a");
     })
 
     it('should return empty string when the string value is empty', () => {
@@ -23,7 +23,7 @@ describe('chop_left string utility', () => {
     })
 
     it('should return original string value when the input character is an empty string', () => {
-        const original_string_value = "abc/def";
+        const original_string_value = "a";
         const input_character = '';
 
         const result = original_string_value.chop_left(input_character);
@@ -32,7 +32,7 @@ describe('chop_left string utility', () => {
     })
 
     it('should return original string value when the input character is null', () => {
-        const original_string_value = "abc/def";
+        const original_string_value = "a";
         const input_character = null;
 
         const result = original_string_value.chop_left(input_character);
@@ -41,7 +41,7 @@ describe('chop_left string utility', () => {
     })
 
     it('should return original string value when the input character is undefined', () => {
-        const original_string_value = "abc/def";
+        const original_string_value = "a";
 
         const result = original_string_value.chop_left();
 
