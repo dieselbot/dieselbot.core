85f470bf489200820023767e51570388c0f9087a
EFS Helper <askmrtillman@gmail.com>
2025-01-06 15:39:15 -0500

    check if google creds exist before init firebase app

--- infrastructure/fuelstop.db.js
+++ infrastructure/fuelstop.db.js
@@ -1,14 +1,18 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
+const google_app_creds_path = process.env.GOOGLE_APPLICATION_CREDENTIALS;
 
-const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+let serviceAccount = null;
 
-initializeApp({
-  credential: cert(serviceAccount)
-});
+if(google_app_creds_path){
+  serviceAccount = require(google_app_creds_path);
+  initializeApp({
+    credential: cert(serviceAccount)
+  });
+}
 
 class FuelStopDB {
-    constructor(firestore = getFirestore()) {
+    constructor(firestore = serviceAccount && getFirestore()) {
         this.firestore = firestore;
     }
 }
