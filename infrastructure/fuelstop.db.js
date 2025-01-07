98daae7dcd6c63c68ab048dddabd9c6662814c80
EFS Helper <askmrtillman@gmail.com>
2025-01-06 22:17:06 -0500

    include config path in error log

--- infrastructure/fuelstop.db.js
+++ infrastructure/fuelstop.db.js
@@ -2,14 +2,16 @@ const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 
 let serviceAccount = null;
+const google_application_credentials = process.env.GOOGLE_APPLICATION_CREDENTIALS;
 
 try {
-  serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+  serviceAccount = require(google_application_credentials);
   initializeApp({
     credential: cert(serviceAccount)
   });
 } catch (error) {
-  console.warn(`failed to init Firebase: `, error);
+  console.warn(`failed to init Firebase: check config file: ${google_application_credentials}`);
+  console.warn(error.message);
 }
 
 class FuelStopDB {
