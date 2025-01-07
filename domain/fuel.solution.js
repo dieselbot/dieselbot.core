ba199bac6d3cdb17347503b8d1d1bd50286abc68
EFS Helper <askmrtillman@gmail.com>
2025-01-07 16:53:33 -0500

    refactor; non-breaking changes

--- domain/fuel.solution.js
+++ domain/fuel.solution.js
@@ -4,22 +4,22 @@ class FuelSolution {
     constructor(fuel_solution_text) {
         this.text = fuel_solution_text;
         this.fuel_stops = new Map();
+        this.lines = null;
     }
-    read() {
-        const _text = this.text;
-
-        const _lines = _text.trim().split('\n')
+    read_lines() {
+        this.lines = this.text.trim().split('\n')
             .map(part => part.trim())
             .filter(part => part.length > 1);
-
-        _lines.forEach((line, i) => {
+    }
+    read() {
+        this.read_lines();
+        this.lines.forEach((line, i) => {
             if (/QTY:\s[A-Z|\d]+/.test(line)) {
-                const fuelStop = new FuelStop(line, _lines[i + 1]);
-                      fuelStop.read();
+                const fuelStop = new FuelStop(line, this.lines[i + 1]);
+                fuelStop.read();
                 this.fuel_stops.set(i, fuelStop);
             }
         })
-
     }
 }
 
