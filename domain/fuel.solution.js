60672f2067aff2f706b92f3d7c7aa1b49c79aad1
EFS Helper <askmrtillman@gmail.com>
2025-01-09 16:01:08 -0500

    refactor to use read_lines utility

--- domain/fuel.solution.js
+++ domain/fuel.solution.js
@@ -1,21 +1,17 @@
 const FuelStop = require('./fuel.stop.js');
+const { read_lines } = require('../common/utils.js')
 
 class FuelSolution {
     constructor(fuel_solution_text) {
         this.text = fuel_solution_text;
         this.fuel_stops = new Map();
-        this.lines = null;
     }
-    read_lines() {
-        this.lines = this.text.trim().split('\n')
-            .map(part => part.trim())
-            .filter(part => part.length > 1);
-    }
+    
     read() {
-        this.read_lines();
-        this.lines.forEach((line, i) => {
+        const lines = read_lines(this.text);
+        lines.forEach((line, i) => {
             if (/QTY:\s[A-Z|\d]+/.test(line)) {
-                const fuelStop = new FuelStop(line, this.lines[i + 1]);
+                const fuelStop = new FuelStop(line, lines[i + 1]);
                 fuelStop.read();
                 this.fuel_stops.set(i, fuelStop);
             }
