86e9ef3dbfb0a01114fd71de1e47280abf060e4a
EFS Helper <askmrtillman@gmail.com>
2025-01-10 15:41:11 -0500

    refactor findplace method to receive fuelstop object

--- services/google.places.js
+++ services/google.places.js
@@ -1,34 +1,39 @@
 const GooglePlacesAPI = require('../infrastructure/google.places.api.js')
 
 class GooglePlacesService {
-    
+
     #_placesAPI;
 
-    constructor(placesAPI = new GooglePlacesAPI()){
+    constructor(placesAPI = new GooglePlacesAPI()) {
         this.#_placesAPI = placesAPI;
     }
 
-    async findPlace(searchText){
-        
-        if(!searchText) return;
+    async findPlace(fuelstop) {
+
+        const searchText = fuelstop.search_phrase;
+
+        if (!searchText) return;
 
         const placeResponse = await this.#_placesAPI.textSearch(searchText);
 
-        if(placeResponse.places && placeResponse.places.length > 1){
+        if (placeResponse.places && placeResponse.places.length > 1) {
             console.warn(`multiple results found for query: "${searchText}"`);
             return;
         }
 
-        if(placeResponse.places && placeResponse.places.length == 1){
+        if (placeResponse.places && placeResponse.places.length == 1) {
             const place = placeResponse.places[0];
+            const { code, city, state, highway, exit } = fuelstop;
             return {
+                code,
                 display_name: place.displayName.text,
-                address: place.formattedAddress
+                address: place.formattedAddress,
+                city, state, highway, exit
             };
         }
 
         console.warn(`no results found for query: "${searchText}"`);
-        
+
     }
 }
 
